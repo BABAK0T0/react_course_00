9b882882ce4c89e7af4a7bcbfd511776793335cd
BABAK0T0 <nrandria@student.42.fr>
2020-01-14 11:22:34 +0100

    Add cards

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -2,14 +2,17 @@ import React from 'react';
 import Brand from './Brand';
 import {Container} from './Container';
 import {GlobalStyle} from './GlobalStyle';
+import {Card} from './Card';
 
 export default function Dashboard() {
     return (
-        <>
+        <React.Fragment>
             <GlobalStyle />
             <Container>
                 <Brand />
+                <Card height={400}>Chart goes here</Card>
+                <Card height={400}>Table goes here</Card>
             </Container>
-        </>
+        </React.Fragment>
     );
 }
