d50432ab40c02c028fc64ed0cce23dfe84390155
BABAK0T0 <nrandria@student.42.fr>
2020-01-14 19:01:35 +0100

    Add ThemeContext and DarkSwitch/GlobalStyle themes

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, {useState,createContext} from 'react';
 import Brand from './Brand';
 import {Container} from './Container';
 import {GlobalStyle} from './GlobalStyle';
 // import {Card} from './Card';
 import {Chart} from './Chart';
 import {VirtualizedTable} from './Tables';
+import { DarkSwitch } from './DarkSwitch';
+
+export const ThemeContext = createContext();
 
 export default function Dashboard() {
+    const themeState = useState('light');
+    const [theme, setTheme] = themeState;
+    const dark = theme === 'dark';
     return (
-        <React.Fragment>
-            <GlobalStyle />
+        <ThemeContext.Provider value={themeState}>
+            <GlobalStyle dark={dark}/>
             <Container>
                 <Brand />
+                <DarkSwitch/>
                 <Chart />
                 <VirtualizedTable/>
             </Container>
-        </React.Fragment>
+        </ThemeContext.Provider>
     );
 }
